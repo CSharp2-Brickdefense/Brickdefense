51f8e5e56b5c347464fb1397da5dbee159e38f5d
Maaike <maaikeveurink@hotmail.com>
2017-06-22 10:40:34 +0200

    some stuff

--- Brickdefense/Game.cs
+++ Brickdefense/Game.cs
@@ -4,12 +4,7 @@ using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-
 using Microsoft.Xna.Framework.Graphics;
-
-using System;
-using System.Collections.Generic;
-
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Windows.System.Threading;
